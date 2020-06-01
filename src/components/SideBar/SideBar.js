20fd2d52819ecae01ae15d7fe7cd1bdfb462a74b
vpalmerini <victorpalmerini@gmail.com>
2020-05-31 21:21:24 -0300

    feat: create useHover hook

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,12 +1,16 @@
 import React from "react";
 
+import useHover from "../../hooks/useHover";
+
 import "./SideBar.css";
 
 const SideBar = () => {
+  const [hoverRef, isHovered] = useHover();
+
   return (
     <>
-      <div className="sideBarOverlay"></div>
-      <nav className="sideBarContainer">
+      <div className={`${isHovered ? "sideBarOverlay" : null}`}></div>
+      <nav className="sideBarContainer" ref={hoverRef}>
         <ul></ul>
       </nav>
     </>
